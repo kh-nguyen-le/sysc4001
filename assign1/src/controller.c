7ee5e91643dc6ed14920cab26f99a19102eed933
JFlood <jordanflood@cmail.carleton.ca>
2015-10-07 18:46:11 -0400

    Wrote psuedocode for controller

---
+++ assign1/src/controller.c
@@ -0,0 +1,10 @@
+#include "common.h"
+#include <event2/event.h>
+
+int main (int argc, char *argv[]) {
+  //TODO: Initialize MSQ
+  //TODO: FORK
+  //TODO: Handshake and register devices in child
+  //TODO: Set up FIFO in parent
+  
+}
