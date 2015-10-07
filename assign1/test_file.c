fb205f73b377c854ef9ee4d792bab71a442949cf
Khoi Nguyen Le <johnle@cmail.carleton.ca>
2015-10-07 01:02:10 -0400

    Update tester to simulate controller child

--- assign1/test_file.c
+++ assign1/test_file.c
@@ -1,9 +1,4 @@
-#include "datatypes.h"
-
-struct my_msg_st {
-    long int my_msg_type;
-    char some_text[BUFSIZ];
-};
+#include "common.h"
 
 void sigint_handler(int sig) {
 	printf("Caught SIGINT\n");
@@ -11,13 +6,10 @@ void sigint_handler(int sig) {
 
 int main (int argc, char *argv[])  {
 
-  int mqid;
-  struct my_msg_st some_data;
-  mqid = msgget (ftok ("./UID", 1), 0666 | IPC_CREAT);
-  printf("%d", mqid);
+  struct device_msg some_data;
+  (void)acquire_msgq();
   int running = 1;
   struct sigaction act;
-  
   act.sa_handler = sigint_handler;
   sigemptyset(&act.sa_mask);
   act.sa_flags = 0;
@@ -25,19 +17,24 @@ int main (int argc, char *argv[])  {
 
   
   while(running)  {
-  if (msgrcv(mqid, (void *)&some_data, BUFSIZ,0, 0) == -1) {
-    fprintf(stdout, "msgrcv failed with error: %d\n", errno);
-    perror("msgrcv");
-    if (errno == EINTR) {
-      printf("Caught signal while blocked on msgrcv(). Retrying... msgrcv()\n");
-        if (msgctl(mqid, IPC_RMID, 0) == -1) {
-          fprintf(stderr, "msgctl(IPC_RMID) failed\n");
-          exit(EXIT_FAILURE);
-        }
-      } else {
-	exit(EXIT_FAILURE);
+    if (msgrcv(mqid, (void *)&some_data, sizeof(some_data.private_info),CONTROLLER_CHILD, 0) == -1) {
+      fprintf(stdout, "msgrcv failed with error: %d\n", errno);
+      perror("msgrcv");
+      if (errno == EINTR) {
+        printf("Caught signal while blocked on msgrcv(). Closing message queue..\n");
+          if (msgctl(mqid, IPC_RMID, 0) == -1) {
+            fprintf(stderr, "msgctl(IPC_RMID) failed\n");
+            exit(EXIT_FAILURE);
+          }
+        } 
+      else {
+	  exit(EXIT_FAILURE);
       } 
     }
+    printf("Name: %s\n",some_data.private_info.name);
+    printf("Type: %c\n",some_data.private_info.device_type);
+    printf("Threshold: %d\n",some_data.private_info.threshold);
+    printf("Current Value: %d\n",some_data.private_info.current_value);
   }           	
 }
 
