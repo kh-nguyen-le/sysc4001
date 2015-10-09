546fe1e0418f698d2893aa83b0d316f5b9792fd0
JFlood <jordanflood@cmail.carleton.ca>
2015-10-08 21:15:32 -0400

    Fixed first if statement in controller.c

---
+++ assign1/src/cloud.c
@@ -0,0 +1,15 @@
+#include "common.h"
+#include <event2/event.h>
+
+
+int main (int argc, char *argv[]) {
+
+
+
+
+
+
+
+
+
+}
--- assign1/src/controller.c
+++ assign1/src/controller.c
@@ -3,6 +3,7 @@
 
 static struct event_base *base;
 static struct ctrl_msg cmd_msg;
+static struct timeval tv;
 GHashTable* hash;
 GArray* database;
 
@@ -13,46 +14,50 @@ void child_duty (evutil_socket_t fd, short events, void *arg) {
   struct hash_entry_t temp;
   int count = 0;
   
-  if (!msgrcv (mqid, (void *)&my_msg, sizeof (struct device_info), CONTROLLER_CHILD, IPC_NOWAIT))  {
-  
-  if (!g_hash_table_lookup_extended (hash, my_msg.private_info.name, (gpointer*)key , (gpointer*)db_index))  {
-    cmd_msg.msg_type = my_msg.private_info.pid;
-    cmd_msg.private_info.command = START_COMMAND;
-    if (msgsnd (mqid, (void*)&cmd_msg, sizeof (struct ctrl_info), 0) == -1) {
+  if (msgrcv (mqid, (void *)&my_msg, sizeof (struct device_info), CONTROLLER_CHILD, 0))  {
+    if (!g_hash_table_lookup_extended (hash, my_msg.private_info.name, (gpointer*)key , (gpointer*)db_index))  {
+      cmd_msg.msg_type = my_msg.private_info.pid;
+      cmd_msg.private_info.command = START_COMMAND;
+      printf ("Sending START command to PID: %d\n", my_msg.private_info.pid);
+      if (msgsnd (mqid, (void*)&cmd_msg, sizeof (struct ctrl_info), 0) == -1) {
         perror ("Message send failed!");
+      }//end if
+      printf ("Sent START command to PID: %d\n", my_msg.private_info.pid);
+      temp.sensor_pid = (my_msg.private_info.device_type == 's') ? my_msg.private_info.pid : 0;
+      temp.actuator_pid = (my_msg.private_info.device_type == 'a') ? my_msg.private_info.pid : 0;
+      temp.info = my_msg.private_info;
+      database = g_array_append_val (database, temp);
+      *db_index = g_array_get_element_size (database);
+      g_hash_table_insert (hash, my_msg.private_info.name, db_index);
     }//end if
-    temp.sensor_pid = (my_msg.private_info.device_type == 's') ? my_msg.private_info.pid : 0;
-    temp.actuator_pid = (my_msg.private_info.device_type == 'a') ? my_msg.private_info.pid : 0;
-    temp.info = my_msg.private_info;
-    database = g_array_append_val (database, temp);
-    *db_index = g_array_get_element_size (database);
-    g_hash_table_insert (hash, my_msg.private_info.name, db_index);
-  }//end if
-  temp = g_array_index (database, struct hash_entry_t, *db_index);
-  if (temp.sensor_pid == 0 ) {
-    temp.sensor_pid = my_msg.private_info.pid;
-    temp.info.threshold = my_msg.private_info.threshold;
-    temp.info.current_value = my_msg.private_info.current_value;
-    g_array_insert_val (database, *db_index, temp);
-  } else if (temp.actuator_pid == 0) {
-    temp.actuator_pid = my_msg.private_info.pid;
-    temp.info.activated = my_msg.private_info.activated;
-    g_array_insert_val (database, *db_index, temp);
-  }//end if
+    temp = g_array_index (database, struct hash_entry_t, *db_index);
+    if (temp.sensor_pid == 0 ) {
+      temp.sensor_pid = my_msg.private_info.pid;
+      temp.info.threshold = my_msg.private_info.threshold;
+      temp.info.current_value = my_msg.private_info.current_value;
+      g_array_insert_val (database, *db_index, temp);
+    } else if (temp.actuator_pid == 0) {
+      temp.actuator_pid = my_msg.private_info.pid;
+      temp.info.activated = my_msg.private_info.activated;
+      g_array_insert_val (database, *db_index, temp);
+    }//end if
     
-  if (my_msg.private_info.device_type == 'a') {
+    if (my_msg.private_info.device_type == 'a') {
       cmd_msg.private_info.command = ACT_COMMAND;
       printf ("Sending ACT command to PID: %d\n", my_msg.private_info.pid);
       printf ("Device name: %s \n PID: %d", my_msg.private_info.name, my_msg.private_info.pid);
       msgsnd (mqid, (void*)&cmd_msg, sizeof (struct ctrl_info), 0);
-  }//end if
+    }//end if
     if (++count > 10) {
       cmd_msg.private_info.command = STOP_COMMAND;
       printf ("Sending STOP command to PID: %d\n", my_msg.private_info.pid);
       printf ("Device name: %s \n PID: %d", my_msg.private_info.name, my_msg.private_info.pid);
       msgsnd (mqid, (void*)&cmd_msg, sizeof (struct ctrl_info), 0);
     }//end if
-  }//end if  
+  } else {
+    perror ("msgrcv");
+    exit (EXIT_FAILURE);
+  }
 }//end function
 
 void parent_duty (evutil_socket_t fd, short events, void *arg) {
@@ -64,8 +69,7 @@ void parent_duty (evutil_socket_t fd, short events, void *arg) {
 
 
 int main (int argc, char *argv[]) {
-  //TODO: Initialize MSQ
-  
+  struct event* ev;
   // parse cmd line arguments
   base = event_base_new();
   pid_t pid;
@@ -74,10 +78,9 @@ int main (int argc, char *argv[]) {
     exit (EXIT_FAILURE);
   }
   
-  sscanf(argv[1], "%s", &cmd_msg.private_info.name);
+  //sscanf(argv[1], "%s", &cmd_msg.private_info.name);
   if (!acquire_msgq ())  exit (EXIT_FAILURE);
     
-  //TODO: FORK
   pid = fork ();
   event_reinit (base);
   if (pid < 0)  {
@@ -89,17 +92,16 @@ int main (int argc, char *argv[]) {
     perror ("Failed to create FIFO!");
     exit (EXIT_FAILURE);
   }
-    struct event *ev = event_new (base, -1, EV_PERSIST, parent_duty, NULL);
-    struct timeval tv = {1,0};
-    event_add (ev, &tv);
+    ev = event_new (base, -1, EV_PERSIST, parent_duty, NULL);
   } else if (pid > 0)  {
     //child
     hash = g_hash_table_new(g_str_hash, g_str_equal);
     database = g_array_new (FALSE, TRUE, sizeof (struct hash_entry_t));
-    struct event *ev = event_new (base, -1, EV_PERSIST, child_duty, NULL);
-    struct timeval tv = {1,0};
-    event_add (ev, &tv);
+    ev = event_new (base, -1, EV_PERSIST, child_duty, NULL);
   }
+  tv.tv_sec = 1;
+  tv.tv_usec = 0;
+  event_add (ev, &tv);
   event_base_dispatch (base);
   //TODO: Handshake and register devices in child
   
